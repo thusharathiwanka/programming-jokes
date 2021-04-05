b2845b1edcfb8197c56797fe2f60089e349961dc
thusharathiwanka <it19076362@my.sliit.lk>
2021-04-06 01:41:24 +0530

    CREATE : api call, data update added

---
+++ app/js/script.js
@@ -0,0 +1,21 @@
+const jokeText = document.querySelector(".joke");
+const jokeBtn = document.querySelector("button");
+const API_URL =
+  "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist";
+
+jokeBtn.addEventListener("click", async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+
+  console.log(data);
+
+  let joke = "";
+
+  if (data.joke === undefined) {
+    joke = `${data.setup} <br/> ${data.delivery}`;
+  } else {
+    joke = `${data.joke}`;
+  }
+
+  jokeText.innerHTML = joke;
+});
